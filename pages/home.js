b843dd532c269642b9242b81d5a88b1256e4e873
Shreyas <shreyxs@gmail.com>
2021-11-09 15:13:26 +0530

    using CSS modules

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import Script from "next/script";
 import React from "react";
 import axios from "axios";
 import Stack from "@mui/material/Stack";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
+import styles from "../styles/home.module.css";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
 	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -32,6 +34,7 @@ export default function Home() {
 				setTheme(mode);
 			});
 		}
+
 		function setTheme(mode) {
 			if (mode == "light") {
 				document.getElementById("theme-style").href = "/default.css";
@@ -48,6 +51,7 @@ export default function Home() {
 			localStorage.setItem("theme", mode);
 		}
 	};
+
 	React.useEffect(() => {
 		checkTheme();
 		(async () => {
@@ -91,8 +95,8 @@ export default function Home() {
 			<Head>
 				<title>Shreyas - Home</title>
 				<link rel="icon" href="/favicon.ico" />
-				<link rel="stylesheet" href="/home.css" />
-				<link id="theme-style" rel="stylesheet" type="text/css" href="" />
+				<link id="theme-style" rel="stylesheet" href="" />
+				<link rel="stylesheet" href="/home-global.css" />
 			</Head>
 
 			<Stack spacing={2} sx={{ width: "100%" }}>
@@ -107,81 +111,94 @@ export default function Home() {
 				</Snackbar>
 			</Stack>
 
-			<section className="s1">
-				<div className="main-container">
-					<div className="greeting-wrapper">
+			<section className={styles.s1}>
+				<div className={styles["main-container"]}>
+					<div className={styles["greeting-wrapper"]}>
 						<h1>Good to see you here!</h1>
 					</div>
-					<div className="intro-wrapper">
-						<div className="nav-wrapper">
+					<div className={styles["intro-wrapper"]}>
+						<div className={styles["nav-wrapper"]}>
 							<a href="#">
-								<div className="dots-wrapper">
-									<div id="dot-1" className="browser-dot"></div>
-									<div id="dot-2" className="browser-dot"></div>
-									<div id="dot-3" className="browser-dot"></div>
+								<div className={styles["dots-wrapper"]}>
+									<div
+										id={styles["dot-1"]}
+										className={styles["browser-dot"]}
+									></div>
+									<div
+										id={styles["dot-2"]}
+										className={styles["browser-dot"]}
+									></div>
+									<div
+										id={styles["dot-3"]}
+										className={styles["browser-dot"]}
+									></div>
 								</div>
 							</a>
-							<ul id="navigation">
+							<ul id={styles.navigation}>
 								<li>
 									<a href="#contact-form">Contact</a>
 								</li>
 							</ul>
 						</div>
-						<div className="left-column">
-							<img alt="Shrey" id="profile_pic" src="/images/shrey.jpg" />
+						<div className={styles["left-column"]}>
+							<img
+								alt="Shrey"
+								id={styles.profile_pic}
+								src="/images/shrey.jpg"
+							/>
 							<h5 style={{ textAlign: "center", lineHeight: 0 }}>
 								Pick your Theme.
 							</h5>
-							<div id="theme-options-wrapper">
+							<div id={styles["theme-options-wrapper"]}>
 								<div
 									data-mode="light"
-									id="light-mode"
-									className="theme-dot"
+									id={styles["light-mode"]}
+									className={"theme-dot"}
 								></div>
 								<div
 									data-mode="blue"
-									id="blue-mode"
-									className="theme-dot"
+									id={styles["blue-mode"]}
+									className={"theme-dot"}
 								></div>
 								<div
 									data-mode="green"
-									id="green-mode"
-									className="theme-dot"
+									id={styles["green-mode"]}
+									className={"theme-dot"}
 								></div>
 								<div
 									data-mode="purple"
-									id="purple-mode"
-									className="theme-dot"
+									id={styles["purple-mode"]}
+									className={"theme-dot"}
 								></div>
 							</div>
-							<p id="settings-note">
+							<p id={styles["settings-note"]}>
 								Theme settings will be saved for
 								<br />
 								your next vist
 							</p>
 						</div>
-						<div className="right-column">
-							<div id="preview-shadow">
-								<div id="preview">
-									<div id="corner-tl" className="corner"></div>
-									<div id="corner-tr" className="corner"></div>
+						<div className={styles["right-column"]}>
+							<div id={styles["preview-shadow"]}>
+								<div id={styles.preview}>
+									<div id={styles["corner-tl"]} className={styles.corner}></div>
+									<div id={styles["corner-tr"]} className={styles.corner}></div>
 									<h3>What I Am</h3>
 									<p>
 										A consistent LEARNER and a self-taught Full-Stack Web
 										Developer.
 									</p>
-									<div id="corner-br" className="corner"></div>
-									<div id="corner-bl" className="corner"></div>
+									<div id={styles["corner-br"]} className={styles.corner}></div>
+									<div id={styles["corner-bl"]} className={styles.corner}></div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className="s2">
-				<div className="main-container">
-					<div className="about-wrapper">
-						<div className="about-me">
+			<section className={styles.s2}>
+				<div className={styles["main-container"]}>
+					<div className={styles["about-wrapper"]}>
+						<div className={styles["about-me"]}>
 							<h4>More about me</h4>
 							<p
 								style={{
@@ -202,7 +219,7 @@ export default function Home() {
 							<hr />
 							<h4>TOP EXPERTISE</h4>
 							<p>Full-Stack developer with primary focus on the MERN Stack.</p>
-							<div id="skills">
+							<div id={styles.skills}>
 								<ul>
 									<li>ReactJS/Redux</li>
 									<li>NextJS</li>
@@ -219,22 +236,22 @@ export default function Home() {
 								</ul>
 							</div>
 						</div>
-						<div className="social-links">
-							<img alt="work" id="social_img" src="/images/work.webp" />
+						<div className={styles["social-links"]}>
+							<img alt="work" id={styles.social_img} src="/images/work.webp" />
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className="s1">
-				<div className="main-container">
+			<section className={styles.s1}>
+				<div className={styles["main-container"]}>
 					<h3 style={{ textAlign: "center" }}>Some of my past projects</h3>
-					<div className="post-wrapper">
+					<div className={styles["post-wrapper"]}>
 						<div>
-							<div className="post">
-								<img className="thumbnail" src="/images/xtreme.png" />
-								<div className="post-preview">
-									<h6 className="post-title">XTREME Gameshop</h6>
-									<p className="post-intro">
+							<div className={styles.post}>
+								<img className={styles.thumbnail} src="/images/xtreme.png" />
+								<div className={styles["post-preview"]}>
+									<h6 className={styles["post-title"]}>XTREME Gameshop</h6>
+									<p className={styles["post-intro"]}>
 										An ecommerce website that&apos;s built on the MERN stack and
 										has all admin controls.
 									</p>
@@ -245,11 +262,11 @@ export default function Home() {
 							</div>
 						</div>
 						<div>
-							<div className="post">
-								<img className="thumbnail" src="/images/logod.png" />
-								<div className="post-preview">
-									<h6 className="post-title">Logo Designer</h6>
-									<p className="post-intro">
+							<div className={styles.post}>
+								<img className={styles.thumbnail} src="/images/logod.png" />
+								<div className={styles["post-preview"]}>
+									<h6 className={styles["post-title"]}>Logo Designer</h6>
+									<p className={styles["post-intro"]}>
 										Built and maintaining the website for a professional Logo
 										Designer from Goa, India.
 									</p>
@@ -260,11 +277,11 @@ export default function Home() {
 							</div>
 						</div>
 						<div>
-							<div className="post">
-								<img className="thumbnail" src="/images/rem.png" />
-								<div className="post-preview">
-									<h6 className="post-title">Reminders & Todos</h6>
-									<p className="post-intro">
+							<div className={styles.post}>
+								<img className={styles.thumbnail} src="/images/rem.png" />
+								<div className={styles["post-preview"]}>
+									<h6 className={styles["post-title"]}>Reminders & Todos</h6>
+									<p className={styles["post-intro"]}>
 										A stylish Progressive Web App that sends out web push
 										notifications and email reminders.{" "}
 									</p>
@@ -280,34 +297,44 @@ export default function Home() {
 					</div>
 				</div>
 			</section>
-			<section className="s2">
-				<div className="main-container">
+			<section className={styles.s2}>
+				<div className={styles["main-container"]}>
 					<a href=""></a>
 					<h3 style={{ textAlign: "center" }}>Get In Touch</h3>
 					<form
 						onSubmit={onSubmit}
 						// action="https://shreyas-saver.herokuapp.com/save"
 						// method="post"
-						id="contact-form"
+						id={styles["contact-form"]}
 					>
 						<label>Name</label>
-						<input className="input-field" type="text" id={"name"} required />
+						<input
+							className={styles["input-field"]}
+							type="text"
+							id={"name"}
+							required
+						/>
 						<label>Subject</label>
 						<input
 							required
-							className="input-field"
+							className={styles["input-field"]}
 							type="text"
 							id={"subject"}
 						/>
 						<label>Email</label>
-						<input className="input-field" required type="email" id={"email"} />
+						<input
+							className={styles["input-field"]}
+							required
+							type="email"
+							id={"email"}
+						/>
 						<label>Message</label>
 						<textarea
-							className="input-field"
+							className={styles["input-field"]}
 							required
 							id={"message"}
 						></textarea>
-						<input id="submit-btn" type="submit" value="Send" />
+						<input id={"submit-btn"} type="submit" value="Send" />
 					</form>
 				</div>
 			</section>
